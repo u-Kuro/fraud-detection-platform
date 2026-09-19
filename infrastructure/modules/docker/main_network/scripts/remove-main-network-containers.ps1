90b091205abc4e28bfb58f944587aa3836182e6c
Reaven Dupitas <dupitas.reaven@gmail.com>
2026-09-20 01:44:16 +0800

    Added all terraform created docker container to exclusion for network cleanup.

--- infrastructure/modules/docker/main_network/scripts/remove-main-network-containers.ps1
+++ infrastructure/modules/docker/main_network/scripts/remove-main-network-containers.ps1
@@ -8,7 +8,7 @@ $main_network_name = $Env:MAIN_NETWORK_NAME
 $main_network_json_configurations = (docker inspect $main_network_name | ConvertFrom-Json)[0]
 $main_network_containers          = $main_network_json_configurations.Containers
 
-$exclude_containers = @("ministack")
+$exclude_containers = @("ministack", "traefik-http-proxy")
 foreach ($container_properties in $main_network_containers.PSObject.Properties) {
     $container = $container_properties.Value
     if ($container.Name -in $exclude_containers) { continue }
