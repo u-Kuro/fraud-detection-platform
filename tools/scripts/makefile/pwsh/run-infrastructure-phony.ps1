4ef8adb43037d0bdda9faddf97e6fd8f0452b956
Reaven Dupitas <dupitas.reaven@gmail.com>
2026-09-20 23:15:00 +0800

    ongoing changes for OS agnostic setup...

--- tools/scripts/makefile/pwsh/run-infrastructure-phony.ps1
+++ tools/scripts/makefile/pwsh/run-infrastructure-phony.ps1
@@ -8,14 +8,6 @@ $ErrorActionPreference = "Stop"
 
 $WORKING_DIRECTORY = "/app"
 
-Write-Host @"
-docker run --rm ``
-    --volume "${Env:ROOT_DIRECTORY}:${WORKING_DIRECTORY}" ``
-    --volume "${Env:DOCKER_SOCK}:/var/run/docker.sock" ``
-    "${Env:INFRASTRUCTURE_IMAGE}" ``
-    "${MAKEFILE_PHONY}"
-"@
-
 docker run --rm `
     --volume "${Env:ROOT_DIRECTORY}:${WORKING_DIRECTORY}" `
     --volume "${Env:DOCKER_SOCK}:/var/run/docker.sock" `
